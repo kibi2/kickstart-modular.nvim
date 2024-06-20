cc46c066b52fdab0025c947b4afa5e066418dd98
OGAWA <70509917+kibi2@users.noreply.github.com>
2024-06-20 15:30:00 +0900

    new file:   fugitive.lua 	new file:   gitgutter.lua 	new file:   hop.lua 	new file:   onenord.lua 	new file:   undotree.lua

---
+++ lua/custom/plugins/hop.lua
@@ -0,0 +1,11 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  'smoka7/hop.nvim',
+  version = '*',
+  opts = {
+    keys = 'etovxqpdygfblzhckisuran',
+  },
+  keys = {
+    { mode = '', '<leader>se', '<cmd>HopWordMW<CR>', desc = 'Go to any word in the current buffer' },
+  },
+}
---
+++ lua/custom/plugins/onenord.lua
@@ -0,0 +1,40 @@
+if true then
+  return {}
+end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+return {
+  'rmehri01/onenord.nvim',
+  -- lazy = true,
+  -- theme = "light",
+  priority = 1000,
+  --opts = {
+  config = function()
+    require('onenord').setup {
+      borders = true,
+      fade_nc = false,
+      styles = {
+        comments = 'NONE',
+        strings = 'NONE',
+        keywords = 'bold',
+        functions = 'bold',
+        variables = 'NONE',
+        diagnostics = 'underline',
+      },
+
+      disable = {
+        background = true,
+      },
+
+      custom_colors = {
+        mypink = '#FFB2CC',
+      },
+      -- local colors = require('onenord.colors').load()
+      custom_highlights = {
+        MatchParen = { style = 'bold,underline' },
+        -- gitsigns 修正
+        GitSignsAddLnInline = { style = 'underline' },
+        GitSignsChangeLnInline = { style = 'underline' },
+        GitSignsDeleteLnInline = { style = 'bold,underline' },
+      },
+    }
+  end,
+}
