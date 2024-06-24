c7cbd5f69932a0190eb6e132f68fdf74d0f076c1
OGAWA <70509917+kibi2@users.noreply.github.com>
2024-06-24 09:13:47 +0900

    key bind に desc を付ける

--- lua/custom/plugins/nvim-hlslens.lua
+++ lua/custom/plugins/nvim-hlslens.lua
@@ -3,54 +3,61 @@ return {
   'kevinhwang91/nvim-hlslens',
   keys = {
     {
-      mode = "n",
-      "n",
+      mode = 'n',
+      'n',
       "<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>",
-      noremap = true, silent = true,
+      noremap = true,
+      silent = true,
     },
     {
-      mode = "n",
-      "N",
+      mode = 'n',
+      'N',
       "<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>",
-      noremap = true, silent = true,
+      noremap = true,
+      silent = true,
     },
     -- vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
     {
-      mode = "n",
-      "*",
+      mode = 'n',
+      '*',
       "*<Cmd>lua require('hlslens').start()<CR>",
-      noremap = true, silent = true,
+      noremap = true,
+      silent = true,
     },
     {
-      mode = "n",
-      "#",
+      mode = 'n',
+      '#',
       "#<Cmd>lua require('hlslens').start()<CR>",
-      noremap = true, silent = true,
+      noremap = true,
+      silent = true,
     },
     {
-      mode = "n",
-      "g*",
+      mode = 'n',
+      'g*',
       "g*<Cmd>lua require('hlslens').start()<CR>",
-      noremap = true, silent = true,
+      noremap = true,
+      silent = true,
     },
     {
-      mode = "n",
-      "g#",
+      mode = 'n',
+      'g#',
       "g#<Cmd>lua require('hlslens').start()<CR>",
-      noremap = true, silent = true,
+      noremap = true,
+      silent = true,
     },
     { -- run `:nohlsearch` and export results to quickfix
-      mode = {"n", "x"},
-      "<Leader>L",
+      mode = { 'n', 'x' },
+      '<Leader>L',
       function()
         vim.schedule(function()
           if require('hlslens').exportLastSearchToQuickfix() then
-            vim.cmd('cw')
+            vim.cmd 'cw'
           end
         end)
         return ':noh<CR>'
       end,
-      expr = true
+      expr = true,
+      desc = '[L]ast Search To Quickfix',
     },
   },
 }
