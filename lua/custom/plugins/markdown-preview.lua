d0c9a019789c3d80e85fc95fcceca6ecf38964fb
OGAWA <70509917+kibi2@users.noreply.github.com>
2024-06-24 09:17:12 +0900

    フォーマット

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,12 +1,21 @@
 -- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
+  'iamcco/markdown-preview.nvim',
+  cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+  build = 'cd app && npm install',
   init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_filetypes = { 'markdown' }
   end,
-  ft = { "markdown" },
+
+  ft = { 'markdown' },
+  keys = {
+    {
+      mode = 'n',
+      'gp',
+      '<cmd>MarkdownPreviewToggle<CR>',
+      desc = 'Markdown Preview Toggle',
+    },
+  },
 }
 --[[
 {
